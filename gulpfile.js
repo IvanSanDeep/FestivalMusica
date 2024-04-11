67656297ec16bdf7fc729f62589867808ff5500b
IvanSanDeep <isd17@comunidad.unam.mx>
2024-04-11 14:17:29 -0600

    Avances del proyecto Festival

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const{ src, dest, watch, parallel }  = require("gulp"); //Forma de extrar la funcionalidad de GULP
+//css
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
+
+function css(done){
+  src("src/scss/**/*.scss")//Identificar  el archivo SASS
+    .pipe(plumber())
+    .pipe( sass() )//Compilar
+    .pipe( dest("build/css"));//Almacenarla en el disco duro
+
+
+  done(); //Callback que aviosa a gul llegamos al final
+}
+
+
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3
+  }
+    src('src/img/**/*.{png,jpg}')
+    .pipe( cache( imagemin(opciones)) )
+    .pipe( dest('build/img'))
+  done();
+}
+
+function versionWebp( done ){
+  const opciones = {
+    quality: 50
+  };
+  src('src/img/**/*.{png,jpg}')
+    .pipe( webp(opciones) )
+    .pipe( dest('build/img'))
+  done();
+}
+
+
+
+function dev(done){
+  watch("src/scss/**/*.scss", css)  
+
+  done();
+}
+
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel (imagenes, versionWebp, dev);
